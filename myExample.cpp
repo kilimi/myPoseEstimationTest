8500258ec8cbf985f2cf2311af13d379b3c44cfd
Lilita Kiforenko <lilita@kiforenko.eu>
2013-11-08 16:08:56 +0100

    trying to get ELS and TEX from point cloud

--- myExample.cpp
+++ myExample.cpp
@@ -5,10 +5,16 @@
 #include <PoseEstimation/Descriptors.h>
 #include <PoseEstimation/DescriptorEstimation.h>
 #include "Features/extendedLineSegment3D.h"
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
 
 using namespace std;
 using namespace PoseEstimation;
 using namespace cv;
+
+typedef DescRGBN DescT;
+void computeELSfromPointCLoud(string xmlName, string rgbFile, string depthFile, string pointCloudFile);
 
 void setTransformation( KVector<> &_t, KMatrix<> &_R, int camera)
 {
@@ -119,7 +125,8 @@ void run(string name, string inputPath)
 	 */
 	std::vector<extendedLineSegment3D> els;
 	DescriptorEstimation de;
-	pair<DescSeg::Vec, DescTex::Vec> temp = de.ecv(rgb, depth, surf, true, true, els);
+	std::vector<texlet3D> tex;
+	pair<DescSeg::Vec, DescTex::Vec> temp = de.ecv(rgb, depth, surf, true, true, els, tex);
 	KVector<> t(3);
 	KMatrix<> R(3, 3);
 	int camera = -1;
@@ -146,22 +153,177 @@ void run(string name, string inputPath)
 	write3DExtendedLineSegmentsToXMLFile(els, outXML.c_str());
 
 	cout<< outXML << " is saved" << endl;
+
 }
+
+
+void justSaveElsAndTex(string xmlName, string rgbFile, string depthFile)
+{
+	cv::Mat_<cv::Vec3b> rgb;
+	cv::Mat_<int> depth;
+	DescECV::Vec surf;
+
+	CameraCalibrationCV cc = CameraCalibrationCV::KinectIdeal();
+
+	DescriptorUtil().loadRGBD(rgbFile, depthFile, rgb,  depth, cc);
+
+	std::vector<extendedLineSegment3D> els;
+	std::vector<texlet3D> tex;
+	DescriptorEstimation de;
+	pair<DescSeg::Vec, DescTex::Vec> temp = de.ecv(rgb, depth, surf, true, true, els, tex);
+
+	string outXML;
+	outXML.append("./xml/");
+	outXML.append(xmlName);
+	outXML.append("_ELS.xml");
+	write3DExtendedLineSegmentsToXMLFile(els, outXML.c_str());
+
+	string outXMLtex;
+	outXMLtex.append("./xml/");
+	outXMLtex.append(xmlName);
+	outXMLtex.append("_TEX.xml");
+	write3DTexletsToXMLFile(tex, outXMLtex.c_str());
+	cout<< outXML << " is saved" << endl;
+
+}
+
+
+
+void test(string name, string inputPath, string rgbFileScene,  string depthFileScene)
+{
+	string _rgb;
+	_rgb.append(name);
+	_rgb.append(".ppm");
+
+	string _depth;
+	_depth.append(name);
+	_depth.append(".png");
+
+	string rgbFile = inputPath + _rgb;
+	string depthFile = inputPath + _depth;
+
+	cv::Mat_<cv::Vec3b> rgb;
+	cv::Mat_<int> depth;
+	DescECV::Vec surf;
+
+	cv::Mat_<cv::Vec3b> rgbScene;
+	cv::Mat_<int> depthScene;
+
+	CameraCalibrationCV cc = CameraCalibrationCV::KinectIdeal();
+
+	DescriptorUtil().loadRGBD(rgbFile, depthFile, rgb,  depth, cc);
+	DescriptorUtil().loadRGBD(rgbFileScene, depthFileScene, rgbScene,  depthScene, cc);
+
+	DescriptorEstimation de;
+	std::pair<DescHist::Vec, DescHist::Vec> hist = de.histogramECV(rgb, depth, 10, 10, false, true);
+	std::pair<DescHist::Vec, DescHist::Vec> histScene = de.histogramECV(rgbScene, depthScene, 50, 10, false, true);
+
+	DescHist::Vec source, target;
+	source = hist.first;
+	target = histScene.first;
+	//	CorrespondenceSearch<DescHist>::Ptr search = CorrespondenceSearch<DescHist>::MakeFeatureNNSearch(target, 2); //MakeXYZRadiusSearch(target, 25.0f);
+	//	search->query(source);
+	//	const Correspondence::Vec& corrs = search->getCorr();
+
+
+	//show texlets
+	//	DescriptorUtil du;
+	//	DescriptorUtil::ViewT view;
+	//	du.addPoints<DescHist>(view, hist.second, "tex_obj");
+	//	du.addPoints<DescHist>(view, histScene.second, "tex_scn");
+	//	du.show(view);
+
+	AlignmentUtil().translate<DescHist>(source, 0, 0, -100);
+	DescriptorUtil().showCorr<DescHist>(source, target, nearestFeatures<DescHist>(source, target), 50);
+}
+
+
+void saveLineSegments3D()
+{
+
+}
+
+
 
 int main(int argc, char* argv[])
 {
 	string inputPath = string("./in/");
 
 	string outputPath = string("./out/");
+	//
+	//	run("top", inputPath);
+	//	run("bottom", inputPath);
+	//	run("right", inputPath);
+	//	run("left", inputPath);
+	//	run("right2", inputPath);
+	//	run("left2", inputPath);
 
-	run("top", inputPath);
-	run("bottom", inputPath);
-	run("right", inputPath);
-	run("left", inputPath);
-	run("right2", inputPath);
-	run("left2", inputPath);
+	//	test("circle_top", inputPath, "./in/circle_rgb.tiff", "./in/circle_depth.tiff");
+
+	//scene
+	//	justSaveElsAndTex("rgb1", "./in/rgb1.tiff", "./in/depth1.tiff");
+
+	//object
+	//	justSaveElsAndTex("rc", "./in/rc_top.ppm", "./in/rc_top.png");
+
+	//object with pointCloud
+
+	computeELSfromPointCLoud("pointCloud", "./in/rc_top.ppm", "./in/rc_top.png", "./in/rc_pointcloud_top.png");
 
 	waitKey(0);
 	return 0;
 }
+
+
+void computeELSfromPointCLoud(string xmlName, string rgbFile, string depthFile, string pointCloudFile)
+{
+	cv::Mat_<cv::Vec3b> rgb;
+	cv::Mat_<int> depth;
+	DescECV::Vec surf;
+
+	CameraCalibrationCV cc = CameraCalibrationCV::KinectIdeal();
+	std::vector<extendedLineSegment3D> els;
+	std::vector<texlet3D> tex;
+	DescriptorEstimation de;
+	DescriptorUtil().loadRGBD(rgbFile, depthFile, rgb,  depth, cc);
+	pcl::PointCloud<pcl::PointXYZ> cloud; // Filled with data to copy
+
+	if (pcl::io::loadPCDFile<pcl::PointXYZ> ("./in/top.pcd", cloud) == -1) //* load the file
+	{
+		PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
+	}
+	std::cout << "Loaded " << endl;
+
+
+	const int h = cloud.height;
+	const int w = cloud.width;
+
+	cv::Mat_<cv::Vec3f> data3D(h, w, cv::Vec3f::all(0.0f));
+	for (int r = 0; r < h; ++r) {
+		for (int c = 0; c < w; ++c) {
+			const pcl::PointXYZ& p = cloud(c,r);
+			if(pcl::isFinite(p)) {
+				data3D(r, c)[0] = p.x;
+				data3D(r, c)[1] = p.y;
+				data3D(r, c)[2] = p.z;
+			}
+		}
+	}
+
+//	pair<DescSeg::Vec, DescTex::Vec> temp = de.ecv(rgb, depth, data3D, surf, true, true, els, tex);
+
+	//		string outXML;
+	//		outXML.append("./xml/");
+	//		outXML.append(xmlName);
+	//		outXML.append("_ELS.xml");
+	//		write3DExtendedLineSegmentsToXMLFile(els, outXML.c_str());
+	//
+	//		string outXMLtex;
+	//		outXMLtex.append("./xml/");
+	//		outXMLtex.append(xmlName);
+	//		outXMLtex.append("_TEX.xml");
+	//		write3DTexletsToXMLFile(tex, outXMLtex.c_str());
+	//		cout<< outXML << " is saved" << endl;
+}
+
 
